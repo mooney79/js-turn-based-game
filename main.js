9dd74ef4196b7a719a37bfa615b2f0cebe4b3cec
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-23 14:50:07 -0400

    more notes added

--- main.js
+++ main.js
@@ -55,20 +55,43 @@ class Game{
 
 /* Basic Mechanics
 Health pools: 500 (modify by Hero and Enemy Types)
+Option: 10
 Avg swings to bring down an enemy: 10
 Avg accuracy: 75%
-Avg damage/swing: about 65
+Avg damage/swing: about 65/1
 Up to 25% variance for specialties
     Exceptions: grunts should be easy to kill, dragon a real challenge
 
 Assignments:
 Kat -- Alien, Dragon
 Nathan -- Jedi, Stormtrooper
-Sam -- Scoundrel, Sith Lord
+Sam -- Scoundrel, Sith Lord -- enemy attack logic
 
 Enemy -- Change to something like
 
-enemyArray = [{id: 0, name: "dragon", health: 1000}]
+enemyArray = [{id: 0, name: "dragon", health: 1000, 
+            encounterChance: 15, specialMove: breathfire},
+        {}]
+
+Can we target the DOM and make Hero shake when damage is inflicted
+on him?
+Can each "attack" trigger a simple animation?
+
+Today:
+Basic logic, basic layout, basic class construction
+Friday:
+Ideally: to have functioning if ugly game
+Saturday:
+special attacks and begin connecting game to the UI
+Sunday:
+Finish connecting the game to the UI and tuning balance
+
+ACHIEVEMENT UNLOCKED: 
+
+Maybe look into APIs and see if they can benefit us with this?
+
+
+
 
 
 */
