aa70118168b640d2c9f58af5891337080e98e923
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-23 16:21:17 -0400

    basic logic implemented

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ Up to 25% variance for specialties
     Exceptions: grunts should be easy to kill, dragon a real challenge
 
 Assignments:
-Kat -- Alien, Dragon
+Kat -- Alien, Dragon breathes fire!
 Nathan -- Jedi, Stormtrooper
 Sam -- Scoundrel, Sith Lord -- enemy attack logic
 
@@ -121,7 +121,89 @@ ACHIEVEMENT UNLOCKED:
 Maybe look into APIs and see if they can benefit us with this?
 
 
-
-
-
 */
+
+const exampleVillainArray = 
+    [{name: 'Stormtrooper',
+    hitChance: 35,
+    damageModifier: 0,
+    health: 3, 
+    },
+    {name: 'Dragon',
+    hitChance:60,
+    damageModifier: 3,
+    health: 15}];
+
+const exampleHero = {
+    name: 'Jedi',
+    hitChance: 75,
+    damageModifier:1,
+    health: 10,
+}
+
+let monsterIndex = 0;
+// let heroHealth = 10;
+
+
+
+function generatePercentage(){
+    return (Math.floor(Math.random() * 100)+1);    
+}
+
+function generateDamage(){  //Needs to eventually pass in either hero or villain 
+                            // to dynamically grab damageModifier
+    let damageMod = exampleVillainArray[monsterIndex].damageModifier;
+    return (Math.floor(Math.random() *2) +1 + damageMod);
+}
+
+function generateDamageHero(){  //Needs to eventually pass in either hero or villain 
+    // to dynamically grab damageModifier
+let damageMod = exampleHero.damageModifier;
+return (Math.floor(Math.random() *2) +1 + damageMod);
+}
+
+function pickEnemy(){
+    let randomNumber = generatePercentage();
+    if (randomNumber > 90) {
+        monsterIndex = 1;
+        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
+    } else {
+        monsterIndex = 0;
+        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
+    } return monsterIndex;
+}
+
+function enemyAttack(enemy){
+    if(generatePercentage() < exampleVillainArray[monsterIndex].hitChance) {        
+        let damage = generateDamage();
+        exampleHero.health -= damage;
+        return `The hero was hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
+    } else
+        return `The ${exampleVillainArray[monsterIndex].name} missed his attack!`;
+}
+
+function heroAttack(hero){
+    if(generatePercentage() < exampleHero.hitChance) {        
+        let damage = generateDamageHero();
+        exampleVillainArray[monsterIndex].health -= damage;
+        return `The ${exampleHero.name} hero hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
+    } else
+        return `The ${exampleHero.name} hero missed his attack!`;
+}
+
+pickEnemy();
+while (exampleHero.health > 0){
+    console.log(heroAttack());
+    console.log(`${exampleVillainArray[monsterIndex].name}  has ${exampleVillainArray[monsterIndex].health}  remaining!`);
+    if (exampleVillainArray[monsterIndex].health > 0) {
+        console.log(enemyAttack());
+    } else {
+        console.log(`${exampleVillainArray[monsterIndex].name} is dead and can't attack`);
+    }
+    console.log(`Hero has ${exampleHero.health} remaining!`);
+    if (exampleVillainArray[monsterIndex].health <= 0) {
+        exampleVillainArray[monsterIndex].health = 3;
+        pickEnemy();
+    }
+    
+}
