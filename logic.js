e0a6637574aa340c02e87dda092f192a78b4fab5
Samuel Mooney <lappletop@Samuels-MacBook-Pro.local>
2021-09-23 22:01:26 -0400

    reworked data to function more smoothly

---
+++ logic.js
@@ -0,0 +1,213 @@
+/* 
+I'm moving game logic to this file so we can have it not cluttering up the main. 
+When we reach the point where we can use it, we can copy it into the main.
+
+And once this file is empty, we can remove the dependency in index.html
+*/
+
+
+///// RANDOM NUMBER GENERATORS /////
+function generatePercentage(){
+    return (Math.floor(Math.random() * 100)+1);    
+}
+
+function generateDamage(damageMod){  
+    return (Math.floor(Math.random() *2) +1 + damageMod);
+}
+
+//////////// CLASSES ////////////
+class Scoundrel {
+    constructor() {
+      this.name = 'Scoundrel';
+      this.health = 8;
+      this.baseMove = "shootBlaster"
+      this.specialMove = "hide";
+      this.accuracyPercent = 85;  
+    }
+    shootBlaster(){
+        if(generatePercentage() < this.accuracyPercent) {    
+            let damageMod = 1;    
+            let damage = generateDamage(damageMod);
+            enemy.health -= damage;
+            return `The ${this.name} hits the ${enemy.name} for ${damage} damage!`
+        } else {
+            return `The ${this.name}'s blaster shot misses the enemy!`;
+        }
+    }
+    hide(){
+        console.log(`The ${this.name} hides from the enemy! To be implemented`);
+    }
+}
+
+  
+class SithLord {
+    constructor() {
+        this.name = 'Sith Lord';
+        this.health = 10;
+        this.id = 1;
+        this.encounterChance = 20;
+        this.basicMove = "lightsaberAttack";
+        this.specialMove = "shootLightning";
+        this.specialMoveChance = 15;
+        this.accuracyPercent = 80;
+    }
+    selectAttack(){
+        if(generatePercentage() < this.specialMoveChance){
+            let damageMod = 3;   
+            return this.shootLightning(damageMod);
+        } else {
+            let damageMod = 1;    
+            return this.lightsaberAttack(damageMod);
+        }
+    }
+    lightsaberAttack(damageMod){
+        if(generatePercentage() < this.accuracyPercent) {    
+            let damage = generateDamage(damageMod);
+            hero.health -= damage;
+            return `The ${this.name} hits the ${hero.name} for ${damage} damage!`
+        } else {
+            return `The ${this.name}'s lightsaber swing misses the hero!`;
+        }
+    }
+    shootLightning(damageMod){
+        if(generatePercentage() < this.accuracyPercent - 30) {     
+            let damage = generateDamage(damageMod);
+            hero.health -= damage;
+            return `The ${this.name} blasts the ${hero.name} with lightning, inflicting ${damage} damage!`
+        } else {
+            return `The ${this.name}'s lightning is dodged by the ${hero.name}!`;
+        }
+    }
+}
+
+class Dragon {
+    constructor() {
+        this.id = 0;
+        this.name = "Dragon";
+        this.health = 15;
+        this.encounterChance = 15;
+        this.basicMove = "clawsAndBite";
+        this.specialMove = "breathfire";
+        this.specialMoveChance = 15;
+        this.accuracyPercent = 90;
+    }
+    selectAttack(){
+        if(generatePercentage() < this.specialMoveChance){
+            let damageMod = 3;   
+            return this.breathFire(damageMod);
+        } else {
+            let damageMod = 2;    
+            return this.clawsAndBite(damageMod);
+        }
+    }
+    clawsAndBite(damageMod){
+        if(generatePercentage() < this.accuracyPercent-40) {    
+            let damage = generateDamage(damageMod);
+            hero.health -= damage;
+            return `The ${this.name} claws and bites the ${hero.name} for ${damage} damage!`
+        } else {
+            return `The ${this.name}'s wild attacks miss the hero!`;
+        }
+    }
+    breathFire(damageMod){
+        if(generatePercentage() < this.accuracyPercent - 60) {     
+            let damage = generateDamage(damageMod);
+            hero.health -= damage;
+            return `The ${this.name} blasts the ${hero.name} with its fiery breath, inflicting ${damage} damage!`
+        } else {
+            return `The ${this.name}'s fiery breath is dodged by the ${hero.name}!`;
+        }
+    }
+  }
+
+
+
+
+//Depending on which button is picked from dropdown, determine 
+//which Class hero becomes
+let hero = new Scoundrel;   
+
+//Enemy encounter rates to be implemented once
+//additional enemies are put in the game.
+
+function pickEnemy(){
+    let randomNumber = generatePercentage();
+    if (randomNumber > 50) {
+        console.log('The hero encountered a Dragon!');
+        return new Dragon;
+    } else {
+        console.log('The hero encountered a hooded figure!');
+        return new SithLord;
+    }
+}
+
+let enemy = pickEnemy();
+console.log(hero.shootBlaster());
+console.log(enemy.selectAttack());
+console.log(`Hero: ${hero.health}, Enemy:${enemy.health}`);
+console.log(hero.shootBlaster());
+console.log(enemy.selectAttack());
+console.log(`Hero: ${hero.health}, Enemy:${enemy.health}`);
+
+
+
+
+
+
+/////// LOGIC TREES //////
+//variable names and such need to be re-tooled to accomodate
+//the class structure
+
+
+/*
+////// RE-TOOL TO ACCOMODATE ENCOUNTER CHANCE, then "let enemy = new Whatever"
+let monsterIndex = 0;
+function pickEnemy(){
+    let randomNumber = generatePercentage();
+    if (randomNumber > 90) {
+        monsterIndex = 1;
+        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
+    } else {
+        monsterIndex = 0;
+        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
+    } return monsterIndex;
+}
+
+//////// SHOULD BE BUILT INTO ENEMY CLASSES
+function enemyAttack(enemy){
+    if(generatePercentage() < exampleVillainArray[monsterIndex].hitChance) {        
+        let damage = generateDamage();
+        exampleHero.health -= damage;
+        return `The hero was hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
+    } else
+        return `The ${exampleVillainArray[monsterIndex].name} missed his attack!`;
+}
+
+//////// SHOULD BE BUILT INTO HERO CLASSES
+function heroAttack(hero){
+    if(generatePercentage() < exampleHero.hitChance) {        
+        let damage = generateDamageHero();
+        exampleVillainArray[monsterIndex].health -= damage;
+        return `The ${exampleHero.name} hero hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
+    } else
+        return `The ${exampleHero.name} hero missed his attack!`;
+}
+
+
+///// AUTOMATES THE FIGHTS.  GOOD FOR TESTING, NOT FOR PLAY.
+pickEnemy();
+while (exampleHero.health > 0){
+    console.log(heroAttack());
+    console.log(`${exampleVillainArray[monsterIndex].name}  has ${exampleVillainArray[monsterIndex].health}  remaining!`);
+    if (exampleVillainArray[monsterIndex].health > 0) {
+        console.log(enemyAttack());
+    } else {
+        console.log(`${exampleVillainArray[monsterIndex].name} is dead and can't attack`);
+    }
+    console.log(`Hero has ${exampleHero.health} remaining!`);
+    if (exampleVillainArray[monsterIndex].health <= 0) {
+        exampleVillainArray[monsterIndex].health = 3;
+        pickEnemy();
+    }    
+}
+*/
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 class Character {
-  //Basic stats?
+  constructor() {
+    this.name = 'Character';
+    this.health = 10;
+  }
 }
 
 class Hero extends Character {
@@ -16,25 +19,24 @@ class Jedi extends Hero {
 }
 
 class Scoundrel extends Hero {
-  //Scoundrel stats plus moves
-  //Name
-  //Any modifiers
-  //Any moves
+  constructor() {
+    this.stats = stats;
+    this.name = 'Scoundrel';
+    this.health = 8;
+    // this.encounterChance = 25;
+    this.baseMove = "shootBlaster"
+    this.specialMove = "hide";
+    this.accuracyPercent = 85;  }
 }
 
 class Alien extends Hero {
-
   constructor() {
     this.stats = stats;
     this.name = 'Alien';
     this.health = 12;
-    this.encounterChance = 25;
+    // this.encounterChance = 25;
     this.specialMove = "beatWithClub";
     this.accuracyPercent = 75;  }
-  //Alien stats plus moves
-  //Name
-  //Any modifiers
-  //Any moves
 }
 
 class Enemy extends Character {
@@ -42,14 +44,9 @@ class Enemy extends Character {
   // Stats?
   //Do we extend classes for each bad guy?
   //Bad guy list: grunt/Stormtroopers, Darth Maul-type, krayt dragon
-  //Kat calls dragon
 }
 
 class Dragon extends Enemy {
-  // what makes up a Dragon?
-  // attack =>
-  // breathesFire
-  // defeats Hero
   constructor() {
       this.id = 0;
       this.name = "Dragon";
@@ -58,10 +55,24 @@ class Dragon extends Enemy {
       this.specialMove = "breathfire";
       this.accuracyPercent = 90;
   }
+}
+
+class SithLord extends Enemy {
+  constructor() {
+      this.id = 1;
+      this.name = "Sith Lord";
+      this.health = 10;
+      this.encounterChance = 20;
+      this.basicMove = "lightsaberAttack";
+      this.specialMove = "shootLightning";
+      this.accuracyPercent = 80;
+      this.damageModifier = 1;
+  }
 
 }
 
 //****BACKUP !!! POSSIBLY HAVE HERO & ENEMY ARRAYS INSTEAD OF CLASSES HERE */
+/*
 const heroesArray = [{
 
 }]
@@ -87,7 +98,7 @@ const enemiesArray = [{
 
   }
 ]
-
+*/
 class Grunt extends Enemy {
 
 }
@@ -140,87 +151,3 @@ Maybe look into APIs and see if they can benefit us with this?
 
 */
 
-const exampleVillainArray = 
-    [{name: 'Stormtrooper',
-    hitChance: 35,
-    damageModifier: 0,
-    health: 3, 
-    },
-    {name: 'Dragon',
-    hitChance:60,
-    damageModifier: 3,
-    health: 15}];
-
-const exampleHero = {
-    name: 'Jedi',
-    hitChance: 75,
-    damageModifier:1,
-    health: 10,
-}
-
-let monsterIndex = 0;
-// let heroHealth = 10;
-
-
-
-function generatePercentage(){
-    return (Math.floor(Math.random() * 100)+1);    
-}
-
-function generateDamage(){  //Needs to eventually pass in either hero or villain 
-                            // to dynamically grab damageModifier
-    let damageMod = exampleVillainArray[monsterIndex].damageModifier;
-    return (Math.floor(Math.random() *2) +1 + damageMod);
-}
-
-function generateDamageHero(){  //Needs to eventually pass in either hero or villain 
-    // to dynamically grab damageModifier
-let damageMod = exampleHero.damageModifier;
-return (Math.floor(Math.random() *2) +1 + damageMod);
-}
-
-function pickEnemy(){
-    let randomNumber = generatePercentage();
-    if (randomNumber > 90) {
-        monsterIndex = 1;
-        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
-    } else {
-        monsterIndex = 0;
-        console.log(`The hero encountered a ${exampleVillainArray[monsterIndex].name}`)
-    } return monsterIndex;
-}
-
-function enemyAttack(enemy){
-    if(generatePercentage() < exampleVillainArray[monsterIndex].hitChance) {        
-        let damage = generateDamage();
-        exampleHero.health -= damage;
-        return `The hero was hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
-    } else
-        return `The ${exampleVillainArray[monsterIndex].name} missed his attack!`;
-}
-
-function heroAttack(hero){
-    if(generatePercentage() < exampleHero.hitChance) {        
-        let damage = generateDamageHero();
-        exampleVillainArray[monsterIndex].health -= damage;
-        return `The ${exampleHero.name} hero hit by the ${exampleVillainArray[monsterIndex].name} for ${damage} damage!`
-    } else
-        return `The ${exampleHero.name} hero missed his attack!`;
-}
-
-pickEnemy();
-while (exampleHero.health > 0){
-    console.log(heroAttack());
-    console.log(`${exampleVillainArray[monsterIndex].name}  has ${exampleVillainArray[monsterIndex].health}  remaining!`);
-    if (exampleVillainArray[monsterIndex].health > 0) {
-        console.log(enemyAttack());
-    } else {
-        console.log(`${exampleVillainArray[monsterIndex].name} is dead and can't attack`);
-    }
-    console.log(`Hero has ${exampleHero.health} remaining!`);
-    if (exampleVillainArray[monsterIndex].health <= 0) {
-        exampleVillainArray[monsterIndex].health = 3;
-        pickEnemy();
-    }
-    
-}
